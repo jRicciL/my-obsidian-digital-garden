9cae8c5b570b90898dee48b5f2c9016a3d643e98
Joel <ricci.lob@gmail.com>
2022-02-06 13:02:26 -0800

    Rb scripts and styles updated

--- _plugins/tags-formater.rb
+++ _plugins/tags-formater.rb
@@ -1,14 +1,17 @@
 # frozen_string_literal: true
 
-# Turns ==something== in Markdown to <mark>something</mark> in output HTML
-
-Jekyll::Hooks.register [:notes], :post_convert do |note|
+Jekyll::Hooks.register [:notes], :pre_render do |note|
   format_tag(note)
 end
 
 def format_tag(note)
   note.content.gsub!(
-    /(\B#\w*[a-zA-Z]+\w*)/, 
+    /\{\{/, 
+    "{ {"
+  )
+
+  note.content.gsub!(
+    /(\B#\w*[a-zA-Z\-]+\w*)/, 
     "<span class='tag'>\\1</span>"
   )
 end
